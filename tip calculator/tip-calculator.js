b87aaee81254d173ad33bd8eb64a7203893d7dab
htradewell <tradewellharry@gmail.com>
2026-08-10 23:28:44 +0100

    Delete tip calculator/tip-calculator.js

--- tip calculator/tip-calculator.js
+++
@@ -1,7 +0,0 @@
-function calculateTip(bill, tipPercentage){
-    const tip = bill*tipPercentage/100;
-    const total = bill+tip;
-    return `your total is ${total} and your tip is ${tip}`;
-}
-const result = calculateTip(50, 20);
-console.log(result);
