e13f3b099b0523835fad83ecd7ab84280c4583fe
Harry Tradewell <tradewellharry@gmail.com>
2026-08-30 15:33:29 +0100

    Project 15: Accordion

--- project-15-accordion/accordion.js
+++ project-15-accordion/accordion.js
@@ -2,6 +2,14 @@ const questions = document.querySelectorAll('.question');
 questions.forEach(question =>{
     question.addEventListener('click', ()=>{
         const answer = question.nextElementSibling;
-        answer.classList.toggle('open');
+        const isOpen = answer.classList.contains('open');
+        questions.forEach(q =>{
+            const ans = q.nextElementSibling;
+            ans.classList.remove('open');
+        })
+        if (!isOpen){
+            const answer = question.nextElementSibling;
+            answer.classList.toggle('open');
+        }
     })
 })
